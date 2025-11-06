75d8050ca553fa4fda9f029a2497f900436735db
Andraž Andolšek Peklaj <andraz.ap@pm.me>
2025-11-06 10:37:39 +0100

    Update demo program

--- ui.py
+++ ui.py
@@ -16,20 +16,21 @@ def setup(state):
         font="./Metropolis-SemiBold.otf")
 
 def draw(gui, state):
-    with gui.Container(position=[0,0], scale=[1,1], color=[0,1,0,1], depth=0):
-        gui.Text('Wow gee', align='right')
+    with gui.Container(position=[0,0], scale=[1,1], color=[0,1,0,1]):
+        with gui.Grid(position=[0,0], scale=[0.5,0.5], rows=[0.3,None], cols=[None, None], gap=[0.1,0.1]) as g:
+            gui.Button(background=[1,0,0,1], **g(0,(0,2))) and print('1')
+            gui.Button(background=[1,1,0,1], **g(1,0)) and print('2')
+        gui.Text('Wow gee', scale=[0.25,0.25])
         
         # we only need to set it once, otherwise it is locked in that state
         force_drawer = None if len(state.setDrawer) == 0 else state.setDrawer.pop()
-        with gui.Drawer('icon', scale=[0.5,0.5], side='right', offset_edge=0.5, offset_content=0.1, color=[1,0.5,0,1], depth=1, force_state=force_drawer) as (grabbed, isOpen):
+        with gui.Drawer('icon', scale=[0.5,0.5], side='top', offset_edge=0.5, offset_content=0.1, color=[1,0.5,0,1], depth=1, force_state=force_drawer) as (grabbed, isOpen):
             gui.Image(icon)
         if gui.Button('Close drawer' if isOpen else 'Open drawer', position=[0, 0.75], scale=[0.25,0.25], color=[0,0,1,1]):
             state.setDrawer.append(not isOpen)
         
-        with gui.Container(position=[0.25,0.5], scale=[0.5,0.5]):
-            gui.Text('Wow gee wilicker')
+        with gui.Container(position=[0.25,0.5], scale=[0.5,0.5], color=[0.5,0.5,0.5,1]):
+            gui.Text('Wow gee wilicker', align='right')
             if gui.Button('Yippie!', color=ViCoS.RED):
                 print('Pressed', state.i)
                 state.i += 1
-        with gui.Container(scale=[0.5,0.5], color=[0,0,0,1]):
-            pass
