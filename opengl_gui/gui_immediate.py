c926540f863b1d0ee3699a400c9af3775e37782f
Andraž Andolšek Peklaj <andraz.ap@pm.me>
2025-11-20 15:03:50 +0100

    Update texture downscaling

--- opengl_gui/gui_immediate.py
+++ opengl_gui/gui_immediate.py
@@ -237,7 +237,7 @@ class Gui():
             glBindTexture(GL_TEXTURE_2D, tex)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
             channels = data.shape[2]
